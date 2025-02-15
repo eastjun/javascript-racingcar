a1376b2d087b95701dde0bf1f9a4e4c2e5ab0f5b
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-16 04:57:08 +0900

    refactor: OutputView 클래스를 함수 기반으로 변경
    - OutputView 클래스 없이 함수 기반으로 출력 기능을 수행하도록 변경
    - 불필요한 클래스 제거로 코드 구조 간소화

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,27 +1,32 @@
 const MESSAGE = {
     RACING_RESULT_TITLE: '\n실행 결과',
     WINNER_RESULT_TITLE: '최종 우승자:',
+};
+
+function printErrorMessage(errorMessage) {
+    console.log(`${errorMessage}\n`);
 }
 
-class OutputView {
-    printErrorMessage(errorMessage){
-        console.log(`${errorMessage}\n`);
-    }
-
-    printResultMessage(){
-        console.log(MESSAGE.RACING_RESULT_TITLE);
-    }
-
-    printOneRoundResult(cars){
-        cars.forEach(car => {
-            console.log(`${car.name} : ${'-'.repeat(car.position)}`);
-        });
-        console.log();
-    }
-
-    printWinners(winners){
-        console.log(`${MESSAGE.WINNER_RESULT_TITLE} ${winners.join(', ')}`);
-    }
+function printResultMessage() {
+    console.log(MESSAGE.RACING_RESULT_TITLE);
 }
+
+function printOneRoundResult(cars) {
+    cars.forEach(car => {
+        console.log(`${car.name} : ${'-'.repeat(car.position)}`);
+    });
+    console.log();
+}
+
+function printWinners(winners) {
+    console.log(`${MESSAGE.WINNER_RESULT_TITLE} ${winners.join(', ')}`);
+}
+
+const OutputView = {
+    printErrorMessage,
+    printResultMessage,
+    printOneRoundResult,
+    printWinners
+};
 
 export default OutputView;
