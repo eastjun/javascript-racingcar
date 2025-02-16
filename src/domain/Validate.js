84c5d76c3ae3bdf22027a6d1c8d09c387f688b89
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-17 04:02:01 +0900

    refactor: Validate 클래스를 함수 기반 모듈료 변경
    - 기존 Validate 클래스를 개별 검증 함수 모음으로 변경

--- src/domain/Validate.js
+++ src/domain/Validate.js
@@ -1,37 +1,48 @@
 import { ERROR_MESSAGE, MAX_NAME_LENGTH } from './constants.js';
-class Validate {
-    isEmpty(input) {
-        if (!input.trim())
-            throw new Error(ERROR_MESSAGE.IS_EMPTY);
-    }
 
-    carNameLength(input) {
-        if (input.trim().length > MAX_NAME_LENGTH)
-            throw new Error(ERROR_MESSAGE.CAR_NAME_LENGTH);
-    }
-
-    isNumber(input) {
-        if (Number.isNaN(input)) {
-            throw new Error(ERROR_MESSAGE.IS_NUMBER);
-        }
-    }
-
-    isPositiveNumber(input) {
-        if (input < 1) {
-            throw new Error(ERROR_MESSAGE.IS_POSITIVE_NUMBER);
-        }
-    }
-
-    isInteger(input) {
-        if (!Number.isInteger(input)) {
-            throw new Error(ERROR_MESSAGE.IS_INTEGER);
-        }
-    }
-
-    carCount(input) {
-        if (input.length < 2) {
-            throw new Error(ERROR_MESSAGE.CAR_COUNT);
-        }
+function isEmpty(input) {
+    if (!input.trim()) {
+        throw new Error(ERROR_MESSAGE.IS_EMPTY);
     }
 }
+
+function carNameLength(input) {
+    if (input.trim().length > MAX_NAME_LENGTH) {
+        throw new Error(ERROR_MESSAGE.CAR_NAME_LENGTH);
+    }
+}
+
+function isNumber(input) {
+    if (Number.isNaN(input)) {
+        throw new Error(ERROR_MESSAGE.IS_NUMBER);
+    }
+}
+
+function isPositiveNumber(input) {
+    if (input < 1) {
+        throw new Error(ERROR_MESSAGE.IS_POSITIVE_NUMBER);
+    }
+}
+
+function isInteger(input) {
+    if (!Number.isInteger(input)) {
+        throw new Error(ERROR_MESSAGE.IS_INTEGER);
+    }
+}
+
+function carCount(input) {
+    if (input.length < 2) {
+        throw new Error(ERROR_MESSAGE.CAR_COUNT);
+    }
+}
+
+const Validate = {
+    isEmpty,
+    carNameLength,
+    isNumber,
+    isPositiveNumber,
+    isInteger,
+    carCount,
+};
+
 export default Validate;
