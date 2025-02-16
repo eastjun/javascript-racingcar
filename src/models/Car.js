e39d0d2245db2ea25825d8570e70c82e0795efa8
dev-dino22 <wlsskdud02@naver.com>
2025-02-17 03:50:34 +0900

    refactor(Car.js): 생성자 name 유효성 검사 추가

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,7 +1,18 @@
+import ERROR from '../constants/Error.js';
+import Validator from '../utils/Validator.js';
+
 class Car {
   constructor(name) {
+    this.validateName(name);
     this.name = name;
     this.position = 0;
+  }
+
+  validateName(name) {
+    const validationResults = Validator.validateCarName(name);
+    if (Object.values(validationResults).some(isError => isError)) {
+      throw new Error(ERROR.MESSAGE.INVALID_CAR_NAME); // ❌ 특정 에러 메시지 출력 없이 종료
+    }
   }
 
   moveForward(randomValue) {
