dd2cc43c3c897dd92053a611eb68f328f353f56d
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-17 03:53:07 +0900

    refactor: readLineAsync 함수를 src/util/readLineAsync.js로 분리
    - InputView 내에 있던 readLineAsync 함수를 별도 파일로 이동하여, 입출력 관련 유틸리티를 분리
    - 관련 모듈에서 import 경로 업데이트

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,27 +1,5 @@
-import readline from "readline";
 import { INPUT_MESSAGE } from './constants.js';
-
-function readLineAsync(query) {
-  return new Promise((resolve, reject) => {
-    if (arguments.length !== 1) {
-      reject(new Error("arguments must be 1"));
-    }
-
-    if (typeof query !== "string") {
-      reject(new Error("query must be string"));
-    }
-
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-    rl.question(query, (input) => {
-      rl.close();
-      resolve(input);
-    });
-  });
-}
+import { readLineAsync } from '../util/readLineAsync.js';
 
 async function readCarNames() {
   return await readLineAsync(INPUT_MESSAGE.READ_CAR_NAME);
