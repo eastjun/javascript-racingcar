8e818e8af016dfe2e1258063f2ca13f55203be4a
youdame <112458620+youdame@users.noreply.github.com>
2025-02-16 17:39:30 +0900

    refactor: car 객체 생성 테스트

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,8 +1,8 @@
 import Car from "../src/domain/Car.js";
 
-test(`자동차 이름이 올바르게 저장 되었는지 테스트`, () => {
+test(`자동차 객체가 잘 생성되는가`, () => {
   const car = new Car("메타의 람보르기니");
-  expect(car.name).toBe("메타의 람보르기니");
+  expect(car).not.toBeUndefined();
 });
 
 test("자동차는 이동할 수 있다", () => {
