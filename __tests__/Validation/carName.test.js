0c6340f4e762128cb7196e2b0231ec5d496e52de
ha-kuku <hakukudh@gmail.com>
2025-02-17 01:44:23 +0900

    refactor: 테스트 이름 기대결과 포함

--- __tests__/Validation/carName.test.js
+++ __tests__/Validation/carName.test.js
@@ -5,21 +5,21 @@ import {
 } from '../../src/Validation/carName.js';
 
 describe('자동차 이름 유효성 검사', () => {
-  test('자동차 이름 길이 5자 이하', () => {
+  test('자동차 이름 길이 5자 이하 통과', () => {
     const carName = 'hakuu';
     expect(() => {
       validateCarsNameLength(carName);
     }).not.toThrow('[Error]');
   });
 
-  test('자동차 이름 길이 5자 초과', () => {
+  test('자동차 이름 길이 5자 초과 오류', () => {
     const carName = 'hakuuu';
     expect(() => {
       validateCarsNameLength(carName);
     }).toThrow('[Error]');
   });
 
-  test('자동차 이름 올바른 형식', () => {
+  test('자동차 이름 올바른 형식 통과', () => {
     const carsName = 'haku,logun';
     expect(() => {
       validateCarNameForm(carsName);
@@ -32,7 +32,7 @@ describe('자동차 이름 유효성 검사', () => {
     ['', '[Error]'],
     [',', '[Error]'],
   ])(
-    '자동차 이름 공백, 미입력, 구분자만 입력한 틀린 형식',
+    '자동차 이름 공백, 미입력, 구분자만 입력한 틀린 형식 오류',
     (input, errorMessage) => {
       expect(() => {
         validateCarNameForm(input);
@@ -40,7 +40,7 @@ describe('자동차 이름 유효성 검사', () => {
     },
   );
 
-  test('자동차 이름 중복', () => {
+  test('자동차 이름 중복 오류', () => {
     const carNames = 'haku,haku';
     expect(() => {
       validateDuplicatedCarName(carNames);
