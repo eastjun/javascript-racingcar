05f4554b231e229379e95be479a21913344b6b95
ahhyun <wndkgus0225@naver.com>
2025-02-17 02:47:00 +0900

    refactor:findWinners 배열 선언에서 let -> const 변경

--- src/domain/findWinners.js
+++ src/domain/findWinners.js
@@ -1,22 +1,8 @@
-// [
-//   {
-//     name: "A",
-//     count: 2
-//   },
-//   {
-//     name: "B",
-//     count: 1
-//   },
-//   {
-//     name: "C",
-//     count: 3
-//   }
-// ];
 function findWinners(cars) {
   const maxAdvanceCount = Math.max(...cars.map((cars) => cars.count));
   const winners = cars.filter((car) => car.count === maxAdvanceCount);
 
-  let winnersName = [];
+  const winnersName = [];
 
   for (let winner of winners) {
     winnersName.push(winner.name);
