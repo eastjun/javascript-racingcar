85a7ab15be97f21a412764ca66af446423ce0efd
sanghee01 <sang6299@naver.com>
2025-02-17 08:41:17 +0900

    test: 테스트코드 파일 정리, 자동차는 이름을 가져야한다.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,7 +1,7 @@
-import Car from '../src/Models/Car.js';
+import Car from '../src/domain/Car.js';
 
-describe('Car 객체를 테스트', () => {
-  test('객체가 잘 생성됐는지 확인한다.', () => {
+describe('경주할 자동차 이름 입력 검증 테스트', () => {
+  test('자동차는 이름을 가져야 한다.', () => {
     // given
 
     // when
@@ -9,35 +9,5 @@ describe('Car 객체를 테스트', () => {
 
     // then
     expect(car.name).toBe('재오');
-  });
-
-  test('자동차 위치 history 저장 테스트', () => {
-    // when
-    const car = new Car('재오');
-    car.updateHistory(true);
-    car.updateHistory(false);
-    car.updateHistory(true);
-    car.updateHistory(false);
-
-    // then
-    expect(car.history).toEqual([1, 1, 2, 2]);
-  });
-});
-
-describe('자동차 이름 입력 검증 테스트', () => {
-  const FAIL_CASE = ['재오상추앵버', '', '재오,상추,상추', '앵버'];
-  const SUCCESS_CASE = ['재오,상추,앵버', '재,상,앵'];
-  test.each(SUCCESS_CASE)('자동차 이름 성공 테스트', (carName) => {
-    // when
-    const carController = new CarController();
-    // then
-    expect(carController.validateCarName(carName)).toEqual(carName.split(','));
-  });
-
-  test.each(FAIL_CASE)('자동차 이름 실패 테스트', (carName) => {
-    // when
-    const carController = new CarController();
-    // then
-    expect(() => carController.validateCarName(carName)).toThrow('[ERROR]');
   });
 });
