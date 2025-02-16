9de5db1a453edf3a3481ae0a256afc4535172907
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-17 04:11:16 +0900

    test: Validate 테스트 코드 업데이트
    - Validate 클래스를 함수 기반 모듈로 전환한 변경 사항에 맞게 테스트 코드 수정

--- __tests__/Validate.test.js
+++ __tests__/Validate.test.js
@@ -1,43 +1,37 @@
 import Validate from '../src/domain/Validate.js'
 
 test("공백 입력 시 에러 발생", () => {
-  const validate = new Validate();
   expect(() => {
-    validate.isEmpty('');
+    Validate.isEmpty('');
   }).toThrow("[ERROR] 공백이 입력되었습니다.");
 })
 
 test("자동차 이름 글자수 5자 초과시 에러 발생", () => {
-  const validate = new Validate();
   expect(() => {
-    validate.carNameLength('ABCDEF');
+    Validate.carNameLength('ABCDEF');
   }).toThrow("[ERROR] 이름 글자수가 5자를 초과하였습니다.");
 })
 
 test("숫자가 아닌 입력 시 에러 발생", () => {
-  const validate = new Validate();
   expect(() => {
-    validate.isNumber(Number("ABCDEF"));
+    Validate.isNumber(Number("ABCDEF"));
   }).toThrow("[ERROR] 숫자가 아닙니다.");
 })
 
 test("0 이하의 숫자 입력 시 에러 발생", () => {
-  const validate = new Validate();
   expect(() => {
-    validate.isPositiveNumber(0);
+    Validate.isPositiveNumber(0);
   }).toThrow("[ERROR] 0 이하의 숫자 입니다.");
 })
 
 test("정수가 아닌 입력 시 에러 발생", () => {
-  const validate = new Validate();
   expect(() => {
-    validate.isInteger(3.1);
+    Validate.isInteger(3.1);
   }).toThrow("[ERROR] 정수가 아닙니다.");
 })
 
 test("1개의 자동차 입력 시 에러 발생", () => {
-  const validate = new Validate();
   expect(() => {
-    validate.carCount(['car1']);
+    Validate.carCount(['car1']);
   }).toThrow("[ERROR] 2개 이상의 자동차를 입력해 주세요.");
 })
