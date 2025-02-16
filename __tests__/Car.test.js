c25cf9d8d62c0cc9d16d6c0932c4a6e49b4ecd98
dlsxjzld <sinjuk1@naver.com>
2025-02-17 03:20:37 +0900

    refactor: 자동차 생성 단위테스트 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -4,6 +4,14 @@ import Car from '../src/Domain/Model/Car.js';
 const MOVE_FORWARD = 4;
 const MOVE_STOP = 3;
 
+describe('자동차 생성', () => {
+  test('자동차 2대 생성하기', () => {
+    const carNames = ['pobi', 'woni'];
+    const cars = carNames.map((carName) => new Car(carName));
+    expect(cars.length).toBe(carNames.length);
+  });
+});
+
 describe('자동차 움직임', () => {
   test('자동차 전진하기', () => {
     const car = new Car('테스트');
