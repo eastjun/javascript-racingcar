b3bd0ac7fa76abf8b214a7da7902123a076ec646
마 <126178440+shuyeon@users.noreply.github.com>
2025-02-18 00:02:44 +0900

    test : Car 내부 잘못된 자동차 이름 유효성 검사 테스트 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,4 +1,5 @@
 import Car from '../src/domain/Car.js';
+import { ERROR_PREFIX, CAR_NAME_LIST_ERROR_MESSAGES, CAR_NAME_CONDITION } from '../src/constants/Constants.js';
 
 describe('Car 클래스 테스트', () => {
   const car = new Car('메타');
@@ -10,5 +11,17 @@ describe('Car 클래스 테스트', () => {
   test('자동차는 이동할 수 있다', () => {
     car.move();
     expect(car.position).toBe(1);
+  });
+
+  test.each(["", "\n", " "])(`자동차 이름이 빈 값인 경우 에러 발생`, (name) => {
+    expect(() => new Car(name)).toThrow(
+      ERROR_PREFIX + ' ' + CAR_NAME_LIST_ERROR_MESSAGES.NAME_LENGTH_MIN
+    );
+  });
+
+  test.each(["메타의 람보르기니", "수이의거북이"])(`자동차 이름이 ${CAR_NAME_CONDITION.LENGTH_MAX}글자를 초과한 경우 에러 발생`, (name) => {
+    expect(() => new Car(name)).toThrow(
+      ERROR_PREFIX + ' ' + CAR_NAME_LIST_ERROR_MESSAGES.NAME_LENGTH_MAX
+    );
   });
 });
