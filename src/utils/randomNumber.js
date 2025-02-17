a6bef0aeefc73aee780074cb267c03fcfa7f7ad0
sanghee01 <sang6299@naver.com>
2025-02-17 10:37:00 +0900

    fix: import에 확장자 안붙여진 부분 붙임

--- src/utils/randomNumber.js
+++ src/utils/randomNumber.js
@@ -1,4 +1,4 @@
-import { RANDOM_RANGE } from '../constants/MAGIC_NUMBER';
+import { RANDOM_RANGE } from '../constants/MAGIC_NUMBER.js';
 
 export function getRandomNumber() {
   return Math.floor(Math.random() * RANDOM_RANGE);
--- src/utils/separator.js
+++ src/utils/separator.js
@@ -1,4 +1,4 @@
-import { SEPARATE_STANDARD } from '../constants/MAGIC_NUMBER';
+import { SEPARATE_STANDARD } from '../constants/MAGIC_NUMBER.js';
 
 export function splitString(string) {
   return string.split(SEPARATE_STANDARD).map((str) => str.trim());
