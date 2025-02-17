b94749aa5613d1c18b7d662a88e62a5b4e82574a
yeongipark <yeongi0111@naver.com>
2025-02-17 21:01:28 +0900

    test: 시도 횟수 정수인지 확인하는 테스트 코드 추가

--- __tests__/TryNumber.test.js
+++ __tests__/TryNumber.test.js
@@ -1,19 +1,29 @@
 import TryNumberValidator from "../src/domains/validators/TryNumberValidator";
+describe("시도 횟수 유효성 검사", () => {
+  test.each([[1], [100]])(
+    "시도횟수는 1회 이상 100회 이하이어야 한다.",
+    (tryNumber) => {
+      expect(() => {
+        TryNumberValidator.validateInputTryNumber(tryNumber);
+      }).not.toThrow();
+    }
+  );
 
-test.each([[1], [100]])(
-  "시도횟수는 1회 이상 100회 이하이어야 한다.",
-  (tryNumber) => {
-    expect(() => {
-      TryNumberValidator.validateInputTryNumber(tryNumber);
-    }).not.toThrow();
-  }
-);
+  test.each([["a"], ["가가"], ["문자열"], [-1]])(
+    "시도 횟수가 양의 정수가 아닌 경우 에러가 발생한다.",
+    (tryNumber) => {
+      expect(() => {
+        TryNumberValidator.validateInputTryNumber(tryNumber);
+      }).toThrow();
+    }
+  );
 
-test.each([[0], [101]])(
-  "시도횟수가 0회 이하 101회 이상인 경우 에러가 발생한다.",
-  (tryNumber) => {
-    expect(() => {
-      TryNumberValidator.validateInputTryNumber(tryNumber);
-    }).toThrow();
-  }
-);
+  test.each([[0], [101]])(
+    "시도횟수가 0회 이하 101회 이상인 경우 에러가 발생한다.",
+    (tryNumber) => {
+      expect(() => {
+        TryNumberValidator.validateInputTryNumber(tryNumber);
+      }).toThrow();
+    }
+  );
+});
