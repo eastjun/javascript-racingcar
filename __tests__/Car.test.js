efb9ff86931eabeafd3d57890a5df85f00192190
deokbin <deokbin@naver.com>
2025-02-17 15:34:48 +0900

    test(Car): 자동차 객체 생성 테스트 수정

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -4,7 +4,7 @@ describe('자동차 테스트', () => {
   test('자동차 객체를 만들 수 있다.', () => {
     const car = new Car('mato');
 
-    expect(car.getName()).toBe('mato');
+    expect(car).toBeInstanceOf(Car);
   });
 
   test('자동차는 전진할 수 있다.', () => {
