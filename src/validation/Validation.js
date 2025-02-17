9e78162ccb0898bef28e54479d0f7a19050da905
hanheel <hanhee630630@gmail.com>
2025-02-17 18:05:18 +0900

    fix : 이름 최대 길이를 담은 상수를 불러옴

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,13 +1,11 @@
-import { SystemConstants } from "../../../javascript-lotto-test/src/constants/SystemConstants.js";
+import { systemSetting } from "../constants/systemSetting.js";
 
 export const Validation = {
   isNameNotEmpty(parsedString) {
     return !parsedString.some((name) => name.length === 0);
   },
   isNameLengthValid(parsedString) {
-    return !parsedString.some(
-      (name) => name.length > SystemConstants.NAME_LIMIT
-    );
+    return !parsedString.some((name) => name.length > systemSetting.NAME_LIMIT);
   },
   isNameNotDuplicate(parsedString) {
     return parsedString.length === new Set(parsedString).size;
