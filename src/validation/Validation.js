4f0a3ca6917bd068778bf992ef1dfced7c560b0d
hanheel <hanhee630630@gmail.com>
2025-02-17 18:04:24 +0900

    refactor : 이름 길이 제한을 상수로 변경

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,9 +1,13 @@
+import { SystemConstants } from "../../../javascript-lotto-test/src/constants/SystemConstants.js";
+
 export const Validation = {
   isNameNotEmpty(parsedString) {
     return !parsedString.some((name) => name.length === 0);
   },
   isNameLengthValid(parsedString) {
-    return !parsedString.some((name) => name.length > 5);
+    return !parsedString.some(
+      (name) => name.length > SystemConstants.NAME_LIMIT
+    );
   },
   isNameNotDuplicate(parsedString) {
     return parsedString.length === new Set(parsedString).size;
