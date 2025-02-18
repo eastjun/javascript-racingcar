532ea5d308c5e2db884b0bac4580430527c35a0a
마 <126178440+shuyeon@users.noreply.github.com>
2025-02-19 01:23:57 +0900

    refactor : Race 테스트 로직 변경

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -2,45 +2,49 @@ import { Race } from '../src/domain/Race.js';
 import Car from '../src/domain/Car.js';
 import { MOVE_CONDITION } from '../src/constants/Constants.js';
 
-let sui;
-let meta;
 
-beforeEach(() => {
-  sui = new Car("수이");
-  meta = new Car("메타");
-})
 
-describe('조건에 따른 자동차 이동 테스트', () => {  
+describe('조건에 따른 자동차 이동 테스트', () => { 
+  let suiCar = new Car("sui")
+
+  beforeEach(()=>{
+    suiCar = new Car("sui")
+  }) 
+  
   test(`randomNumber가 ${MOVE_CONDITION}  이상이면 자동차가 움직여야 한다.`, () => {
-    Race.moveCar(MOVE_CONDITION, sui)
+    Race.moveCar(MOVE_CONDITION, suiCar)
 
-    expect(sui.position).toBe(1);
+    expect(suiCar.position).toBe(1);
   });
 
   test(`randomNumber가 ${MOVE_CONDITION} 미만이면 자동차가 움직이지 않아야 한다.`, () => {
-    Race.moveCar(MOVE_CONDITION-1,sui)
+    Race.moveCar(MOVE_CONDITION-1, suiCar)
 
-    expect(sui.position).toBe(0);
+    expect(suiCar.position).toBe(0);
   });
 });
 
 
 
-describe('조건에 따른 자동차 이동 테스트', () => {
+describe('우승자 판별 테스트', () => {
   test('공동 우승자', () => {
-    const carList = [sui, meta]
-    Race.moveCar(MOVE_CONDITION, sui)
-    Race.moveCar(MOVE_CONDITION, meta)
+    const carList = [
+      { name: "수이", position: 2 },
+      { name: "메타", position: 2 },
+    ];
+
     const winners = Race.getWinnerName(carList)
 
     expect(winners).toEqual(['수이', '메타'])
   });
 
-  test('단일 우승자', () => {
-    const carList = [sui, meta]
-    Race.moveCar(MOVE_CONDITION, sui)
+  test('단일 우승자', () => {    
+    const carList = [
+      { name: "수이", position: 2 },
+      { name: "메타", position: 1 },
+    ];
     const winners = Race.getWinnerName(carList)
-
+   
     expect(winners).toEqual(['수이'])
   });
   })
