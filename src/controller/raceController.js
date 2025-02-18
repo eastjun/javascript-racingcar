bca083914af97f4f9ec9c7897917da9724d8cc8c
hanheel <hanhee630630@gmail.com>
2025-02-18 21:13:02 +0900

    refactor : raceController에서 winner를 반환

--- src/controller/raceController.js
+++ src/controller/raceController.js
@@ -1,9 +1,13 @@
-import { SystemMessage } from "../constants/SystemMessage";
-import CarRacing from "../domain/CarRacing";
+import { SystemMessage } from "../constants/SystemMessage.js";
+import CarRacing from "../domain/CarRacing.js";
+import { OutputView } from "../view/OutputView.js";
 
 export function startRace(cars, round) {
   OutputView.printMessage(SystemMessage.RESULT_MESSAGE);
   const Race = new CarRacing(cars, round);
   const roundResult = Race.playRace(cars);
-  OutputView.printRound(roundResult);
+  OutputView.printRoundResult(roundResult);
+
+  const Winner = Race.getWinner(cars);
+  return Winner;
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import { raceInit } from "./service/raceService.js";
 import { OutputView } from "./view/OutputView.js";
-import { getWinner } from "./service/statisticsService.js";
 import { startRace } from "./controller/raceController.js";
 
 const { cars, round } = await raceInit();
 
-startRace(cars, round);
-
-const winner = getWinner(cars);
+const winner = startRace(cars, round);
 
 OutputView.printWinner(winner);
