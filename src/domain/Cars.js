2d2e180a05e53775399a56be0ceb7adc75b12948
H0ngJu <hongju4385@gmail.com>
2025-02-19 20:04:46 +0900

    refactor: cars 도메인에서 자동차 이름 유효성 검사 로직을 진행하도록 수정

--- src/domain/Cars.js
+++ src/domain/Cars.js
@@ -1,9 +1,11 @@
+import { ERROR_MESSAGE } from "../constants/ErrorMessage.js";
 import { NUMBER } from "../constants/Number.js";
 
 class Cars {
   #cars;
 
   constructor(carNames) {
+    this.validateCarNames(carNames);
     this.#cars = carNames.map((name) => ({
       name: name,
       position: NUMBER.INIT_POSITION,
@@ -42,6 +44,59 @@ class Cars {
 
   getCars() {
     return [...this.#cars];
+  }
+
+  validateCarNames(carNames) {
+    carNames.forEach((name) => this.#validateEachCarName(name));
+
+    this.#validateDuplicateName(carNames);
+    this.#validateCarNamesLength(carNames);
+  }
+
+  #validateEachCarName(carName) {
+    this.#valiateCarNameLength(carName);
+    this.#validateSpecialSymbol(carName);
+  }
+
+  #valiateCarNameLength(carName) {
+    if (
+      carName.length < NUMBER.MIN_CAR_NAME_LENGTH ||
+      carName.length > NUMBER.MAX_CAR_NAME_LENGTH
+    ) {
+      throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
+    }
+  }
+
+  #validateSpecialSymbol(carName) {
+    const regExp = /^[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]$/g;
+
+    if (regExp.test(carName)) {
+      throw new Error(ERROR_MESSAGE.INVALID_NAME);
+    }
+  }
+
+  #validateCarNamesLength(carNames) {
+    if (
+      carNames.length < NUMBER.MIN_CAR_LIST_LENGTH ||
+      carNames.length > NUMBER.MAX_CAR_LIST_LENGTH
+    ) {
+      throw new Error(ERROR_MESSAGE.INVALID_CARS_LENGTH);
+    }
+  }
+
+  #validateDuplicateName(carNames) {
+    const nameSet = new Set();
+    carNames.forEach((name) => {
+      this.#checkDuplicate(name, nameSet);
+    });
+  }
+
+  #checkDuplicate(carName, nameSet) {
+    if (nameSet.has(carName)) {
+      throw new Error(ERROR_MESSAGE.DUPLICATED_NAME);
+    }
+
+    nameSet.add(carName);
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,7 +5,6 @@ class InputView {
   async getCarNames(message) {
     const inputCarNames = await readLineAsync(message);
     const carNames = inputCarNames.split(",").map((carName) => carName.trim());
-    this.validateCarNames(carNames);
     return carNames;
   }
 
@@ -28,59 +27,6 @@ class InputView {
     if (Number(tryCount) < NUMBER.MIN_TRY_COUNT) {
       throw new Error(ERROR_MESSAGE.TRY_COUNT_NOT_POSITIVE);
     }
-  }
-
-  validateCarNames(carNames) {
-    carNames.forEach((name) => this.#validateEachCarName(name));
-
-    this.#validateDuplicateName(carNames);
-    this.#validateCarNamesLength(carNames);
-  }
-
-  #validateEachCarName(carName) {
-    this.#valiateCarNameLength(carName);
-    this.#validateSpecialSymbol(carName);
-  }
-
-  #valiateCarNameLength(carName) {
-    if (
-      carName.length < NUMBER.MIN_CAR_NAME_LENGTH ||
-      carName.length > NUMBER.MAX_CAR_NAME_LENGTH
-    ) {
-      throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
-    }
-  }
-
-  #validateSpecialSymbol(carName) {
-    const regExp = /^[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]$/g;
-
-    if (regExp.test(carName)) {
-      throw new Error(ERROR_MESSAGE.INVALID_NAME);
-    }
-  }
-
-  #validateCarNamesLength(carNames) {
-    if (
-      carNames.length < NUMBER.MIN_CAR_LIST_LENGTH ||
-      carNames.length > NUMBER.MAX_CAR_LIST_LENGTH
-    ) {
-      throw new Error(ERROR_MESSAGE.INVALID_CARS_LENGTH);
-    }
-  }
-
-  #validateDuplicateName(carNames) {
-    const nameSet = new Set();
-    carNames.forEach((name) => {
-      this.#checkDuplicate(name, nameSet);
-    });
-  }
-
-  #checkDuplicate(carName, nameSet) {
-    if (nameSet.has(carName)) {
-      throw new Error(ERROR_MESSAGE.DUPLICATED_NAME);
-    }
-
-    nameSet.add(carName);
   }
 }
 
