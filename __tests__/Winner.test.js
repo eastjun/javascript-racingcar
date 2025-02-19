f05009babeb2bfc922ead36bdd6f798ff5505a4e
H0ngJu <hongju4385@gmail.com>
2025-02-19 19:22:47 +0900

    test: Cars 통합으로 인한 우승자 테스트 코드 수정

--- __tests__/Winner.test.js
+++ __tests__/Winner.test.js
@@ -1,40 +1,35 @@
-import Car from "../src/domain/Car.js";
-import Winner from "../src/domain/Winner.js";
+import { SYSTEM_MESSAGE } from "../src/constants/SystemMessage.js";
+import Cars from "../src/domain/Cars.js";
 
 describe("자동차 리스트 클래스 테스트", () => {
   describe("자동차 리스트 클래스 정상 케이스", () => {
     const names = ["목성이", "화성이", "금성이"];
-    let winner;
-
-    test("우승자 객체를 생성할 수 있다.", () => {
-      expect(winner).not.toBeUndefined();
-    });
+    let cars;
 
     beforeEach(() => {
-      winner = new Winner(names);
+      cars = new Cars(names);
     });
 
     test("우승자가 한 명일 수 있다.", () => {
-      const carList = [new Car("목성이"), new Car("수성이")];
+      const carList = cars.getCars();
 
-      carList[1].move();
-      carList[1].move();
-      carList[0].move();
+      carList[0].position += 2;
+      carList[1].position += 1;
+      carList[2].position += 5;
 
-      const winners = winner.getWinners(carList, 2);
-      expect(winners).toEqual(["수성이"]);
+      const winners = cars.getWinners();
+      expect(winners).toEqual(["금성이"]);
       expect(winners.length).toBe(1);
     });
 
     test("우승자가 한 명 이상일 수 있다.", () => {
-      const carList = [new Car("목성이"), new Car("수성이")];
+      const carList = cars.getCars();
 
-      carList[1].move();
-      carList[1].move();
-      carList[0].move();
-      carList[0].move();
-
-      const winners = winner.getWinners(carList, 2);
+      carList[0].position += 2;
+      carList[1].position += 5;
+      carList[2].position += 5;
+      const winners = cars.getWinners();
+      expect(winners).toEqual(["화성이", "금성이"]);
       expect(winners.length).toBe(2);
     });
   });
