88f0eaf13410fcf2bc0172b62fc2546703223d86
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-21 00:47:02 +0900

    refactor: 클래스 구조를 함수 모듈로 변경
    - 기존 RacingCar 클래스를 객체를 반환하는 함수 모듈로 변경

--- src/domain/RacingCar.js
+++ src/domain/RacingCar.js
@@ -1,40 +1,43 @@
 import Car from './Car.js';
 import { getRandomNumber } from '../util/random.js';
 
-class RacingCar {
-    createCars(carNames) {
-        return carNames.map(name => new Car(name));
-    }
+function createCars(carNames) {
+    return carNames.map(name => new Car(name));
+}
 
-    oneRound(cars) {
-        cars.forEach(car => {
-            car.move(getRandomNumber(0, 10));
-        });
-        return cars;
-    }
+function oneRound(cars) {
+    cars.forEach(car => {
+        car.move(getRandomNumber(0, 10));
+    });
+    return cars;
+}
 
-    #getMaxPosition(cars) {
-        const hasMaxPositionCar = cars.reduce((prev, value) => {
-            if (prev.position >= value.position) {
-                return prev;
-            } else {
-                return value;
-            }
-        });
-        return hasMaxPositionCar.position;
-    }
+function getMaxPosition(cars) {
+    const hasMaxPositionCar = cars.reduce((prev, value) => {
+        if (prev.position >= value.position) {
+            return prev;
+        } else {
+            return value;
+        }
+    });
+    return hasMaxPositionCar.position;
+}
 
-    getWinners(cars) {
-        const maxPosition = this.#getMaxPosition(cars);
-        const winners = [];
-        cars.forEach(car => {
-            if (car.position === maxPosition) {
-                winners.push(car.name);
-            }
-        });
-        return winners;
-    }
+function getWinners(cars) {
+    const maxPosition = getMaxPosition(cars);
+    const winners = [];
+    cars.forEach(car => {
+        if (car.position === maxPosition) {
+            winners.push(car.name);
+        }
+    });
+    return winners;
+}
 
+const RacingCar = {
+    createCars,
+    oneRound,
+    getWinners,
 }
 
 export default RacingCar;
