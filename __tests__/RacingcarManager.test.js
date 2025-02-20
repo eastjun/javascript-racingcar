dd5886c725d1f98f06160a81ce7abd2ada162781
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-21 00:47:40 +0900

    test: Racing 함수 모듈 구조에 맞게 테스트 코드 수정
    - import RacingCar from './RacingCar.js'에서 new RacingCar() 방식 제거
    - 함수 호출 방식 수정 (RacingCar.createCars() 형태로 변경)

--- __tests__/RacingcarManager.test.js
+++ __tests__/RacingcarManager.test.js
@@ -2,18 +2,15 @@ import Car from '../src/domain/Car';
 import RacingCar from "../src/domain/RacingCar.js";
 
 test("경주에 참여할 자동차 cars 객체 배열이 생성된다.", () => {
-  const racingCar = new RacingCar();
-
   const carNames = ["car1", "car2", "car3"];
 
-  const cars = racingCar.createCars(carNames);
+  const cars = RacingCar.createCars(carNames);
   cars.forEach(car => {
     expect(car).toBeDefined();
   })
 });
 
 test("[최종 우승자 구하기]: 최종 우승자가 1명인 경우.", () => {
-  const racingCar = new RacingCar();
   const car1 = new Car("car1");
   const car2 = new Car("car2");
   const car3 = new Car("car3");
@@ -23,13 +20,12 @@ test("[최종 우승자 구하기]: 최종 우승자가 1명인 경우.", () => 
   car3.position = 1;
 
   const cars = [car1, car2, car3];
-  const winners = racingCar.getWinners(cars);
+  const winners = RacingCar.getWinners(cars);
 
   expect(winners).toEqual(['car1']);
 });
 
 test("[최종 우승자 구하기]: 최종 우승자가 2명 이상인 경우.", () => {
-  const racingCar = new RacingCar();
   const car1 = new Car("car1");
   const car2 = new Car("car2");
   const car3 = new Car("car3");
@@ -39,7 +35,7 @@ test("[최종 우승자 구하기]: 최종 우승자가 2명 이상인 경우.",
   car3.position = 1;
 
   const cars = [car1, car2, car3];
-  const winners = racingCar.getWinners(cars);
+  const winners = RacingCar.getWinners(cars);
 
   expect(winners).toEqual(['car1', 'car2']);
 });
