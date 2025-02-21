d0ab05d2cdd88ef44e2f5f098d80719746773e31
sanghee01 <sang6299@naver.com>
2025-02-22 00:49:33 +0900

    test: tryCountValidates 테스트 케이스 추가

--- __tests__/tryCountValidates.test.js
+++ __tests__/tryCountValidates.test.js
@@ -7,7 +7,19 @@ describe('경주 시도 횟수 입력 검증 테스트', () => {
     }).not.toThrow('[ERROR]');
   });
 
+  test.each([0, 21])('시도 횟수는 1~20 사이가 아닌 경우 에러가 발생한다.', (tryCount) => {
+    expect(() => {
+      checkTryCountRange(tryCount);
+    }).toThrow('[ERROR]');
+  });
+
   test('시도 횟수는 자연수로 입력해야 한다.', () => {
+    const tryCount = 3;
+    expect(() => {
+      checkIsInteger(tryCount);
+    }).not.toThrow('[ERROR]');
+  });
+  test('시도 횟수를 자연수로 입력하지 않으면 에러가 발생한다.', () => {
     const tryCount = 1.2;
     expect(() => {
       checkIsInteger(tryCount);
