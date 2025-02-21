5efa93a3b02b1ebcf94ae4e3ae1096ed84bca9fa
sanghee01 <sang6299@naver.com>
2025-02-21 23:22:37 +0900

    test: Car 객체 테스트

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,10 +1,17 @@
-import { CAR_MOVE_STANDARD } from '../src/constants/MAGIC_NUMBER.js';
 import Car from '../src/domain/Car.js';
 
-describe('경주할 자동차 이름 입력 검증 테스트', () => {
+describe('Car 객체 테스트', () => {
   test('자동차는 이름을 가져야 한다.', () => {
     const car = new Car('재오');
 
     expect(car.name).toBe('재오');
+  });
+
+  test('자동차는 움직일 수 있다.', () => {
+    const car = new Car('재오');
+
+    car.move();
+
+    expect(car.position).toBe(1);
   });
 });
