02b390e1321cfbdedf9bc8fb486f32c55fbaa289
sanghee01 <sang6299@naver.com>
2025-02-21 23:53:58 +0900

    test: Race 객체 테스트

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -1,11 +1,37 @@
-import Car from '../src/domain/Car';
-import { CAR_MOVE_STANDARD } from '../src/constants/MAGIC_NUMBER';
+import Race from '../src/domain/Race';
+import Car from '../src/domain/Car.js';
 
-describe('자동차가 잘 움직이는지 테스트', () => {
-  const car = new Car('상추');
+describe('Race 객체 테스트', () => {
+  test('자동차 리스트가 잘 생성되는가', () => {
+    const cars = ['상추', '재오', '앵버'].map((name) => new Car(name));
+    const race = new Race(cars, 4);
+    expect(race.cars.length).toBe(3);
+    expect(race.tryCount).toBe(4);
+  });
 
-  car.move(4 >= CAR_MOVE_STANDARD);
-  test('자동차는 4 이상의 숫자를 받으면 전진할 수 있다.', () => {
-    expect(car.position).toBe(1);
+  test('tryMove() 실행 후 시도 결과가 history에 잘 저장되는가', () => {
+    const cars = ['상추', '재오', '앵버'].map((name) => new Car(name));
+    const race = new Race(cars, 3);
+
+    race.tryMove();
+
+    expect(race.carPositionHistory.getHistory('상추').length).toBe(3);
+  });
+
+  test('가장 멀리 떨어진 위치에 있는 자동차가 우승자다.', () => {
+    const cars = ['상추', '재오', '앵버'].map((name) => new Car(name));
+    const race = new Race(cars, 3);
+
+    cars[0].move();
+    cars[0].move();
+    cars[0].move();
+    cars[1].move();
+    cars[1].move();
+    cars[1].move();
+    cars[2].move();
+
+    const winners = race.getWinner();
+
+    expect(winners).toEqual(['상추', '재오']);
   });
 });
